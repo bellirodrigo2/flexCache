638918e3b483f133956e21ab568b327cbf8f14f7
bellirodrigo2 <bellirodrigo2@gmail.com>
2024-02-27 12:16:07 -0300

    comp1

--- src/commons.c
+++ src/commons.c
@@ -48,36 +48,3 @@ void time_fx_add(time_fx t1, time_fx t2, time_fx *td){
     td->tv_sec = sec;
     td->tv_nsec = nsec;
 }
-
-bool_t time_fx_is_later(time_fx t1, time_fx t2){
-
-    if(t1.tv_sec < t2.tv_sec)
-        return 0;
-
-    if(t1.tv_sec == t2.tv_sec)
-        return t1.tv_nsec > t2.tv_nsec;
-
-    return 1;
-    
-}
-
-FX_INLINE void time_fx_add_seconds(time_fx t1, long t2, time_fx *td){
-
-    td->tv_sec = t2 + t1.tv_sec;
-}
-
-void time_fx_sub(time_fx t1, time_fx t2, time_fx *td)
-{
-    td->tv_nsec = t2.tv_nsec - t1.tv_nsec;
-    td->tv_sec  = t2.tv_sec - t1.tv_sec;
-    if (td->tv_sec > 0 && td->tv_nsec < 0)
-    {
-        td->tv_nsec += NS_PER_SECOND;
-        td->tv_sec--;
-    }
-    else if (td->tv_sec < 0 && td->tv_nsec > 0)
-    {
-        td->tv_nsec -= NS_PER_SECOND;
-        td->tv_sec++;
-    }
-}
